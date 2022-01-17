88f812ddea4a022216b1b3707b830d9f298435a6
Alvin Fungai <alvin.fungai@gmail.com>
2022-01-17 10:29:01 +0200

    added Todo model

---
+++ app/src/main/java/com/alvinfungai/todoapp/task/domain/model/Todo.kt
@@ -0,0 +1,29 @@
+package com.alvinfungai.todoapp.task.domain.model
+
+import android.graphics.Color
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.res.colorResource
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.alvinfungai.todoapp.R
+import com.alvinfungai.todoapp.ui.theme.Amber700
+import com.alvinfungai.todoapp.ui.theme.BlueGray200
+import com.alvinfungai.todoapp.ui.theme.Green500
+import com.alvinfungai.todoapp.ui.theme.Red500
+import java.sql.Timestamp
+
+
+@Entity
+data class Todo(
+    @PrimaryKey val id: Int? = null,
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val flagColor: Int,
+    val isCheckedOff: Boolean = false
+) {
+    companion object {
+        val noteFlagColors = listOf(Red500, Amber700, Green500, BlueGray200)
+    }
+}
--- app/src/main/java/com/alvinfungai/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/alvinfungai/todoapp/ui/theme/Color.kt
@@ -6,3 +6,9 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// custom colors
+val Red500 = Color(0xFFF44336)
+val Amber700 = Color(0xFFFFA000)
+val Green500 = Color(0xFF4CAF50)
+val BlueGray200 = Color(0xFFB0BEC5)
